b497455274c7f3f8a1b1dda3db45f18e4dd70545
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-08 16:01:59 +0900

    #2. 자식 클래스에서 별도의 메서드나 속성을 추가할 수 있다.

---
+++ p08_inheritance2.py
@@ -0,0 +1,56 @@
+class Robot:
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        Robot.population += 1
+
+    def say_hi(self):
+        print(f"Greetings, my masters call me {self.name}.")
+
+    def cal_add(self, a, b):
+        return a + b
+
+    def die(self):
+        print(f"{self.name} is being destroyed!")
+        Robot.population -= 1
+        if Robot.population == 0:
+            print(f"{self.name} was the last one.")
+        else:
+            print(f"There are still {Robot.population} robots working.")
+
+    @classmethod
+    def how_many(cls):
+        print(f"We have {cls.population} robots.")
+
+    @staticmethod
+    def are_you_robot():
+        print(f"Robot.population: {Robot.population}")
+        print("yes!")
+
+    def __str__(self):
+        return f"{self.name} robot!!"
+
+    def __call__(self):
+        print("call")
+        print(f"{self.name} call!!")
+
+
+class Siri(Robot):
+    def call_me(self):
+        print("네?")
+
+    def cal_mul(self, a, b):
+        self.a = a
+        return a * b
+
+    @classmethod
+    def hello_apple(cls):
+        print(f"cls: {cls}")
+
+
+siri = Siri("iPhone 14 Pro")
+siri.call_me()  # 네?
+print(siri.cal_mul(7, 8))  # 56
+print(siri.a)
+Siri.hello_apple()  # cls: <class '__main__.Siri'>
