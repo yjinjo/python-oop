851c0136de3dc8017ea28d548ab031669fd3a8cc
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-09 09:07:41 +0900

    #2. 다형성(polymorphism)

---
+++ p14_polymorphism.py
@@ -0,0 +1,46 @@
+class Robot:
+    __population = 0
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        Robot.__population += 1
+
+    @property
+    def get_name(self):
+        return f"yoon {self.__name}"
+
+    @property
+    def get_age(self):
+        return self.__age
+
+    @get_age.setter
+    def set_age(self, new_age):
+        if new_age < 0:
+            raise TypeError("invalid range to age")
+        self.__age = new_age
+
+    def __say_hi(self):
+        print(f"Greetings, my masters call me {self.__name}.")
+
+    def cal_add(self, a, b):
+        return a + b
+
+    @classmethod
+    def how_many(cls):
+        print(f"We have {cls.population} robots.")
+
+
+class Siri(Robot):
+    def say_apple(self):
+        print("hello my apple")
+
+
+class SiriKo(Robot):
+    def say_apple(self):
+        print("안녕하세요")
+
+
+class Bixby(Robot):
+    def say_samsung(self):
+        print("안녕하세요")
